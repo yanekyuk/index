b60ed3c3743a6c0eb72036c28d29a52d08a73c6d
Yankı Ekin Yüksel <yanki@aposto.com>
2025-12-01 22:42:07 +0300

    chore: remove debug files and add CLI test suite

---
+++ protocol/src/cli/test-data.ts
@@ -0,0 +1,20 @@
+export const PRIVY_TEST_ACCOUNTS = [
+    { name: 'Casey Harper', email: 'test-6285@privy.io', phoneNumber: '+1 555 555 1625', otpCode: '607027' },
+    { name: 'Devon Brooks', email: 'test-9716@privy.io', phoneNumber: '+1 555 555 2920', otpCode: '670543' },
+    { name: 'Morgan Li', email: 'test-1761@privy.io', phoneNumber: '+1 555 555 5724', otpCode: '888893' },
+    { name: 'Riley Nguyen', email: 'test-5331@privy.io', phoneNumber: '+1 555 555 6283', otpCode: '094228' },
+    { name: 'Taylor Singh', email: 'test-6462@privy.io', phoneNumber: '+1 555 555 8175', otpCode: '066860' },
+    { name: 'Quinn Ramirez', email: 'test-7106@privy.io', phoneNumber: '+1 555 555 8469', otpCode: '991478' },
+    { name: 'Emerson Blake', email: 'test-6945@privy.io', phoneNumber: '+1 555 555 9096', otpCode: '510460' },
+    { name: 'Peyton Alvarez', email: 'test-2676@privy.io', phoneNumber: '+1 555 555 9419', otpCode: '503536' },
+    { name: 'Sydney Clarke', email: 'test-7561@privy.io', phoneNumber: '+1 555 555 9497', otpCode: '737681' },
+    { name: 'Hayden Moore', email: 'test-1093@privy.io', phoneNumber: '+1 555 555 9779', otpCode: '934435' },
+];
+
+export const INTENTS = [
+    'Looking for AI researchers to collaborate on machine learning projects',
+    'Seeking blockchain developers for DeFi partnerships',
+    'Connecting with designers for UI/UX collaborations',
+    'Building cross-functional teams for product development',
+    'Open to mentoring junior developers in web3 space',
+];
